83323cabd99632de8dae226f15cb3b6545eaf786
amangoding <kesumaaa34@gmail.com>
2026-05-12 22:12:31 +0800

    update javascript
    sebenarnya masih ada kekurangan juga di versi desktopnya, yaitu saat menekan enter tetapi yasudahlah wkwk

--- script.js
+++ script.js
@@ -11,25 +11,24 @@ function halo() {
 function discoverBtn() {
     const greet = document.getElementById('greet').value
 
-    // document.getElementById('discover').textContent = 'halo aingnua'
-
-
     if (greet == '') {
         const discover = document.getElementById('discoverResult')
 
-        const konten = document.createElement('p')
+        discover.innerHTML = '<p>harap masukkan nama anda!</p>'
 
-        const isikonten = document.createTextNode('harus memasukkan nama!')
+        // const konten = document.createElement('p')
 
-        konten.append(isikonten)
+        // const isikonten = document.createTextNode('harus memasukkan nama!')
 
-        discover.append(konten)
+        // konten.append(isikonten)
 
-        // document.getElementById('discoverResult').textContent = 'harus memasukkan nama!'
+        // discover.append(konten)
     } else {
         const discover = document.getElementById('discoverResult')
 
         const konten = document.createElement('p')
+
+        discover.innerHTML = '<p></p>' 
 
         const isikonten = document.createTextNode('halo ' + greet)
 
@@ -37,8 +36,33 @@ function discoverBtn() {
 
         discover.append(konten)
     }
-
 }
+
+const greeting = document.getElementById('greet')
+let greet = document.getElementById('greet').value
+
+greeting.addEventListener('keydown', function (e) {
+    let greet = document.getElementById('greet').value
+
+    if (e.key === 'Enter') {
+        e.preventDefault()
+
+        const discover = document.getElementById('discoverResult')
+        
+        discover.innerHTML = "<p></p>"
+        
+        const konten = document.createElement('p')
+        
+        const isikonten = document.createTextNode('halo ' + greet)
+        
+        konten.append(isikonten)
+        
+        discover.append(konten)
+        
+        discoverBtn();
+    }
+})
+
 
 const app = document.querySelector('#welcome-element');
 
@@ -49,17 +73,3 @@ const kontenHeader = document.createTextNode('')
 header.append(kontenHeader)
 
 app.append(header)
-
-// const discover = document.getElementById('discover')
-
-// const konten = document.createElement('p')
-
-// const isikonten = document.createTextNode('halo anunya!')
-
-// konten.append(isikonten)
-
-// discover.append(konten)
-
-
-// window.prompt('halo siapa namamu?')
-// console.log("halo dunia!")
